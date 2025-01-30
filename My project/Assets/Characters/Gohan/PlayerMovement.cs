e22a64f4d08f6f2187ac6082240a795c83fe89d0
KDEM0 <k.demertzidis@sintlucasedu.nl>
2025-01-30 15:51:43 +0100

    Dragon Kombat Z
    Retro Fighting game like Mortal Kombat but with Dragon Ball Characters

---
+++ My project/Assets/Characters/Gohan/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float horizontalInput;
+    float moveSpeed = 6f;
+    float jumpPower = 6f;
+    bool isGrounded = false;
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isGrounded = false;
+            animator.SetBool("isJumping", !isGrounded);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y); 
+        animator.SetFloat("xVelocity", Math.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+        animator.SetBool("isJumping", !isGrounded);
+    }
+}
